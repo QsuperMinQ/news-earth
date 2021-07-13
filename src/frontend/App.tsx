145f85b7180fc3dc56415c4eee6af540a82208e6
Wei Liu <71244325+weiliu9304@users.noreply.github.com>
2021-07-13 18:41:24 +0800

    feat: integrate with map (#4)

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
 import GlobeView from './globe/GlobeView';
+import MapView from './map/MapView';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 type Props = {}
 
 const App: React.FC<Props> = () => {
   return (
     <div className="App">
-      <GlobeView></GlobeView>
+      <Router>
+        <Switch>
+          <Route path="/globe">
+            <GlobeView />
+          </Route>
+          <Route path="/mapView">
+            <MapView />
+          </Route>
+          <Route path="/">
+            <GlobeView />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
