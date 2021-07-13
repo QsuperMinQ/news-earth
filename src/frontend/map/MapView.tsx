70e032f114282a34b26097d87bba7fa5345525f5
Wei Liu <wei.liu@smartnews.com>
2021-07-13 18:40:24 +0800

    feat: integrate with map

---
+++ src/frontend/map/MapView.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import mapboxgl from 'mapbox-gl';
+import './MapStyle.css';
+
+type Props = {}
+
+const MapView: React.FC<Props> = () => {
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoid2VpbGl1OTMiLCJhIjoiY2tyMXRyeXRpMjVpdDJvcWh4ZGI0MTJ1NyJ9.US2-VirgCQkgLL3onAfqJw';
+    const map = new mapboxgl.Map({
+      container: "mapView",
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-70.9, 42.35],
+      zoom: 9
+    });
+  }, []);
+  return <div id ="mapView" className="mapStyle" ></div>
+}
+
+export default MapView;
