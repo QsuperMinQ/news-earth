3323e7007d40407897c401d464f3f6183029b7c8
Wei Liu <wei.liu@smartnews.com>
2021-07-15 11:03:03 +0800

    chore: fallback to js

--- src/frontend/globe/GlobeView.js
+++ src/frontend/globe/GlobeView.js
@@ -5,10 +5,10 @@ import { Cartesian3 } from "cesium";
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 const pointGraphics = { pixelSize: 10 };
 
-const GlobeView = () => (
-  <Viewer full>
-    <Entity position={position} point={pointGraphics} />
-  </Viewer>
-);
+function GlobeView() {
+  return <Viewer full>
+          <Entity position={position} point={pointGraphics} />
+         </Viewer>
+}
 
 export default GlobeView;
